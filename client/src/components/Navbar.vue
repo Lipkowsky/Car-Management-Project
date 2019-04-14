9549cc839c63f68ee6212f85eb5fe2e0e729cf39
PiotrLipkowski <Piotrek789150@gmail.com>
2019-04-14 23:07:44 +0200

    new navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,30 +1,37 @@
 <template>
-
-  <header>
-    <div class="container">
-      <nav>
-        <h1 class="brand"><router-link class="nav-link" to="/">Car <span>Management </span>Project</router-link></h1>
-        <ul>
-          <li v-if="auth==''" class="nav-item">
-            <router-link class="nav-link" to="/login">Logowanie</router-link>
-          </li>
-          <li v-if="auth==''" class="nav-item">
-            <router-link class="nav-link" to="/register">Rejestracja</router-link>
-          </li>
-          <li v-if="auth=='loggedin'" class="nav-item">
-            <router-link class="nav-link" to="/profile">Profil</router-link>
-          </li>
-          <li v-if="auth=='loggedin'" class="nav-item">
-            <a class="nav-link" href="" v-on:click="logout">Wyloguj</a>
-          </li>
-        </ul>
-      </nav>
+  <nav>
+    <div class="toggle">
+      <font-awesome-icon class="menu" icon="bars" />
     </div>
-  </header>
+    <ul>
+      <li v-if="auth==''"><router-link class="nav-link" to="/">Car Management Project</router-link></li>
+      <li v-if="auth==''"><router-link class="nav-link" to="/login">Logowanie</router-link></li>
+      <li v-if="auth==''"><router-link class="nav-link" to="/register">Rejestracja</router-link></li>
+      <li v-if="auth=='loggedin'"><router-link class="nav-link" to="/profile">Profil</router-link></li>
+      <li v-if="auth=='loggedin'"><a class="nav-link" href="" v-on:click="logout">Wyloguj</a></li>
+    </ul>
+  </nav>
 </template>
 
 
 <script>
+  import Vue from 'vue'
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faBars } from '@fortawesome/free-solid-svg-icons'
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  // We import JQuery
+  const $ = require('jquery');
+  // We declare it globally
+  window.$ = $;
+
+
+
+
+  library.add(faBars)
+  Vue.component('font-awesome-icon', FontAwesomeIcon)
+  Vue.config.productionTip = false
+
+
 import EventBus from './EventBus'
   EventBus.$on('logged-in', test =>{
     console.log(test);
@@ -33,14 +40,14 @@ import EventBus from './EventBus'
     data() {
       return {
         auth: '',
-        user: ''
+        user: '',
       }
     },
     methods: {
       logout () {
         localStorage.removeItem('usertoken')
         this.auth = '';
-      }
+      },
     },
       mounted() {
         EventBus.$on('logged-in', status => {
@@ -51,112 +58,69 @@ import EventBus from './EventBus'
         }else{
           this.auth = ''
         }
-      },
+        //hamburger open on media
+        $(document).ready(function () {
+          $('.menu').click(function(){
+            $('ul').toggleClass('active')
+          })
+        })
+      }
   }
 </script>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css?family=Oswald');
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  a{
-    text-decoration: none;
-    color: #eee;
-  }
-
-  header{
-    width: 100%;
-    height: 7vh;
-    background-image: linear-gradient(#010a0c,#23454f,#3f5a60);
-    background-size: cover;
-    font-size: 1.2rem;
-  }
-  .container{
-    max-width:120rem;
-    width: 90%;
-    margin: 0 auto;
-  }
   nav{
-    padding-top: 0.5rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    text-transform: uppercase;
-    font-family: 'Oswald', sans-serif;
+     margin: 0;
+     padding: 0;
+     font-family: sans-serif;
+     width: 100%;
+     background: #00316b;
+    color: #fff;
+   }
+  ul{
+    width: 80%;
+    margin: 0 auto;
+    padding: 0;
   }
-  .brand{
-    font-size: 3rem;
-    font-weight: 300;
-    transform: translateX(-100rem);
-    animation: slideIn .5s forwards;
-  }
-  .brand span{
-    color: #06d7e9;
-  }
-  nav ul{
-    display: flex;
-  }
-  nav ul li{
+  ul li{
     list-style: none;
-    transform: translateX(100rem);
-    animation: slideIn .5s forwards;
+    display: inline-block;
+    padding: 20px;
   }
-  nav ul li:nth-child(1){
-    animation-delay: 0s;
+  .nav-link:hover{
+    background: #e91e63;
   }
-  nav ul li:nth-child(2){
-    animation-delay: 0.5s;
+  .nav-link
+  {
+   background-image: linear-gradient(#601146, #e91e63);
+    color:#fff;
+    text-decoration: none;
   }
-  nav ul li:nth-child(3){
-    animation-delay: 1s;
-  }
-  nav ul li:nth-child(4){
-    animation-delay: 1.5s;
-  }
-  nav ul li a{
-    padding: 1rem 0;
-    margin: 0 3rem;
-    position: relative;
-    letter-spacing: 2px;
-  }
-  nav ul li a:last-child{
-    margin-right: 0;
-  }
-  nav ul li a::before,
-  nav ul li a::after{
-    content: '';
-    position: absolute;
+  .toggle{
     width: 100%;
-    height: 2px;
-    background-color: crimson;
-    left: 0;
-    transform: scaleX(0);
-    transition: all .5s;
+    padding: 10px 20px;
+    background: #001f44;
+    text-align: right;
+    box-sizing: border-box;
+    font-size: 30px;
+    display: none;
   }
-  nav ul li a::before{
-    top: 0;
-    transform-origin: left;
-  }
-  nav ul li a::after{
-    bottom: 0;
-    transform-origin: right;
-  }
-
-  nav ul li a:hover::before,
-  nav ul li a:hover::after{
-    transform: scaleX(1);
-  }
-
-  @keyframes slideIn {
-    from{
-
+  @media (max-width: 768px){
+    .toggle{
+      display: block;
     }
-    to{
-      transform: translateX(0);
+    ul{
+      width: 100%;
+      display: none;
     }
+    ul li{
+      display: block;
+      text-align: center;
+    }
+    .active{
+      display: block;
+    }
+
   }
 
 </style>
